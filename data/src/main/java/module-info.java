f4074161743ebb50a2603edee91800ed0efce9d3
1999684 <ft021118@163.com>
2024-04-26 14:16:55 +0800

    Initial commit

---
+++ data/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.data {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.data to javafx.fxml;
+    exports org.example.data;
+}
